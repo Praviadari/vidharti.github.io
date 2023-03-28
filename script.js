b9b13b1b82b61ce65b26b47360476d298c7b9ce4
Praviadari <91617978+Praviadari@users.noreply.github.com>
2023-03-28 16:03:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let input=() =>{
+    document.getElementsByClassName(".input")
+    let store=onclick(".add")
+}
+
+let add=() =>{
+    document.getElementsByClassName(".button")
+}
+
+let delete1=() =>{
+    document.getElementsByClassName(".delete")
+}
+let sort=() =>{
+    document.getElementsByClassName(".sort")
+    sort.getElementById("display")
+
+}
+let clear=() =>{
+    document.getElementsByClassName(".clear")
+}
+
+
+let display=document.getElementById("display")
+print(input)
